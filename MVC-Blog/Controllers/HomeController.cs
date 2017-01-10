d8fd3ef4ce24aff8d2f3cb941a434569831c7908
Stefan Georgiev Stefanov <stefandreadlorda@gmail.com>
2017-01-11 00:48:14 +0200

    MVC-BLOG

---
+++ MVC-Blog/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Blog.Models;
+using System.Data.Entity;
+namespace MVC_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var latestPosts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(3);
+
+            return View(latestPosts);
+        }
+    }
+}
